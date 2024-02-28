9ea4c24bd4d4980f3927920850c94b0aa2f08fca
Ana Oliveira <121502538+Jenemebous@users.noreply.github.com>
2024-02-28 11:20:06 -0300

    Add files via upload

---
+++ testes/letra.js
@@ -0,0 +1,61 @@
+/* Crie uma função em JavaScript
+ que recebe uma lista de palavras e retorna uma nova lista contendo apenas as 
+ palavras que começam com a letra 'A' (maiúscula). Utilize programação funcional, 
+ aproveitando funções como filter e map. */
+
+/*
+ const listar = ["caminha", "sobra", "Antilope", "mouse"];
+ 
+ const pegar  = listar.filter((item, i) => {
+    if(item[0] === 'A'){
+        return "Esta é a nova lista " + item
+    }
+ 
+
+ });
+
+console.log(pegar)
+
+*/
+
+/*Crie uma função em JavaScript que recebe uma lista de números
+e retorna a média dos números pares. Utilize programação funcional,
+ aproveitando funções como filter*/
+
+/*
+const numeros = [1, 5, 10, 15, 20, 2, 8];
+
+const pares = numeros.filter((numero ) => {
+    if(numero % 2 === 0){
+        return numero
+
+    }
+});
+let soma = 0
+for(let i=0; i<pares.length; i++){
+    soma = soma + pares[i];
+}
+
+media = soma / pares.length
+
+console.log(pares)
+console.log(soma)
+console.log(media)
+*/
+
+
+/*Crie uma função em JavaScript que recebe uma lista de palavras 
+e retorna uma nova lista contendo 
+o comprimento de cada palavra.
+Utilize programação funcional, aproveitando funções como map.
+*/
+
+/*
+const lista = ["caminha", "sobra", "Antilope", "mouse"];
+
+const novaLista =  lista.map((item) => {
+    return item.length
+})
+console.log(novaLista);
+
+*/
